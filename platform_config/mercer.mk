13ebf980976c4ea71c93a1cda9f938dd924f0ae5
Julian Panetta <15096451+jpanetta@users.noreply.github.com>
2015-08-11 14:28:18 -0400

    updated mercer platform config

--- platform_config/mercer.mk
+++ platform_config/mercer.mk
@@ -1,15 +1,22 @@
-VCG_INC=$(HOME)/libraries/vcglib
-DLIB_INC=$(HOME)/usr/include
-LIBMATHEVAL_INC=$(HOME)/usr/include
-LIBMATHEVAL_LIB=$(HOME)/usr/lib
-INCLUDES=-I$(HOME)/CSGFEM -isystem $(EIGEN_INC) -isystem $(SUITESPARSE_INC) -isystem $(BOOST_INC) -isystem $(LIBMATHEVAL_INC)
-LIBS= -L$(BOOST_LIB) -lboost_program_options -lboost_filesystem -lboost_system \
-	-L$(SUITESPARSE_LIB) -lsuitesparse \
+# Locations of library includes and shared libraries should be configured
+# through environment variables:
+# CSGFEM EIGEN_INC SUITESPARSE_INC SUITESPARSE_LIB BOOST_INC BOOST_LIB CERES_INC
+# CERES_LIB LIBMATHEVAL_INC LIBMATHEVAL_LIB VCGLIB_INC DLIB_INC CLIPPER_INC
+# LEVMAR_INC LEVMAR_LIB TRIANGLE_LIB
+SUITESPARSE_LFLAGS=-L$(SUITESPARSE_LIB) -lsuitesparse \
 	-L$(METIS_LIB) -lmetis \
-	-L$(MKL_LIB) -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread \
-	-L$(LIBMATHEVAL_LIB) -lmatheval \
-	-L$(HOME)/usr/lib64 -lceres -lglog -lgflags
+	-L$(MKL_LIB) -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread
+BOOST_LFLAGS=-L$(BOOST_LIB) -lboost_program_options -lboost_filesystem -lboost_system
+CERES_LFLAGS=-L$(CERES_LIB) -lceres -lglog -lgflags
+LIBMATHEVAL_LFLAGS=-L$(LIBMATHEVAL_LIB) -lmatheval
+CLIPPER_LFLAGS=-L$(CLIPPER_PATH)/lib -lpolyclipping
+LEVMAR_LFLAGS=$(LEVMAR_LIB)/liblevmar.a
+TRIANGLE_LFLAGS=-L$(TRIANGLE_LIB) -ltriangle
 
 CXX=g++
 CC=gcc
+# workaround: link -lsuitesparse-mklstatic, which is available with module
+# "SuiteSparse 4.4.2"
 # CPPFLAGS=-fopenmp (currently breaks cholmod :()
+# CPPFLAGS=-fopenmp
+CPPFLAGS+=-mno-avx -msse4.2
