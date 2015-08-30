e1727a058fe09a34c8bec00e189e8a777f0e7921
Morteza H.S <mhs11@nyu.edu>
2015-08-29 21:48:38 -0400

    further edits to ubuntu.mk

--- platform_config/ubuntu.mk
+++ platform_config/ubuntu.mk
@@ -1,9 +1,9 @@
 CSGFEM_INC=$(HOME)/Project_3DP/CSGFEM
 CSGFEM=$(CSGFEM_INC)
 EIGEN_INC=$(EIGEN3_INCLUDE_DIR)
-SUITESPARSE_INC=/usr/include/suitesparse
 BOOST_INC=/usr/include
 BOOST_LIB=/usr/lib
+SUITESPARSE_INC=/usr/include/suitesparse
 SUITESPARSE_LIB=/usr/lib
 OPTPP_INC=/opt/local/include
 OPTPP_LIB=/opt/local/lib
@@ -11,9 +11,18 @@ CERES_INC=/usr/local/include
 CERES_LIB=/usr/local/lib
 LIBMATHEVAL_INC=/usr/include
 LIBMATHEVAL_LIB=/usr/lib
-VCG_INC=/usr/local/include/vcglib
-DLIB_INC=/usr/local/include/dlib
 
+LEVMAR_INC=/usr/local/include
+LEVMAR_LIB=/usr/local/lib
+
+CLIPPER_INC=$(CLIPPER_PATH)/include/polyclipping
+CLIPPER_LIB=$(CLIPPER_PATH)/lib
+
+TRIANGLE_INC=$(TRIANGLE_PATH)
+TRIANGLE_LIB=$(TRIANGLE_PATH)
+
+VCGLIB_INC=/usr/local/include/vcglib
+DLIB_INC=/usr/local/include/dlib
 
 # INCLUDES=-I$(CSGFEM_INC) -I$(EIGEN_INC) -I$(SUITESPARSE_INC) -I$(CLIPPER_PATH) -I$(TRIANGLE_PATH) \
 #	       -I$(BOOST_INC) $(CERES_INC) -I$(LIBMATHEVAL_INC)
@@ -22,6 +31,11 @@ BOOST_LFLAGS=-L$(BOOST_LIB) -lboost_program_options -lboost_filesystem -lboost_s
 SUITESPARSE_LFLAGS=-lcholmod -lumfpack -llapack -lblas -lpthread
 CERES_LFLAGS=-L$(CERES_LIB) -lceres -lcxsparse -lglog -lgflags
 LIBMATHEVAL_LFLAGS=-L$(LIBMATHEVAL_LIB) -lmatheval
+
+CLIPPER_LFLAGS=-L$(CLIPPER_LIB) -lpolyclipping
+TRIANGLE_LFLAGS=-L$(TRIANGLE_LIB) -ltriangle
+LEVMAR_LFLAGS=-L$(LEVMAR_LIB) -llevmar
+
 
 # LIBS=-L$(BOOST_LIB) -lboost_program_options -lboost_filesystem -lboost_system \
 # 	 -lumfpack -llapack -lblas -lpthread\
