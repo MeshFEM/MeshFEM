399b6294bb77e6da91ff3a6c05fc0679a12a7631
Julian Panetta <15096451+jpanetta@users.noreply.github.com>
2015-10-16 17:08:52 -0400

    added missing sin_top.bc Laplace bc

--- experiments/elasticity_convergence/plot.sh
+++ experiments/elasticity_convergence/plot.sh
@@ -1,3 +1,10 @@
-gnuplot -e "mesh='square'; bc_name='sin'; error_type=0; exact='-1.5039245804035226e-01'" plot.gpi
-gnuplot -e "mesh='square'; bc_name='sin'; error_type=1; exact= '4.9245103853636690e-01'" plot.gpi
-gnuplot -e "mesh='square'; bc_name='sin'; error_type=2; exact= '5.1490379372360950e-01'" plot.gpi
+#!/usr/bin/env zsh
+for p in sin/*(/); do
+    p=$(basename $p)
+       exact_ux=$(tail -n1 sin/$p/deg_2/0.75,0.75.txt | cut -f3)
+       exact_uy=$(tail -n1 sin/$p/deg_2/0.75,0.75.txt | cut -f4)
+    exact_unorm=$(tail -n1 sin/$p/deg_2/0.75,0.75.txt | cut -f5)
+    gnuplot -e "mesh='square'; bc_name='sin'; poisson='$p'; error_type=0; exact='$exact_ux'"    plot.gpi
+    gnuplot -e "mesh='square'; bc_name='sin'; poisson='$p'; error_type=1; exact='$exact_uy'"    plot.gpi
+    gnuplot -e "mesh='square'; bc_name='sin'; poisson='$p'; error_type=2; exact='$exact_unorm'" plot.gpi
+done
