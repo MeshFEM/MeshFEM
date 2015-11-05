f6f4de7deaf8d96e437af707eb7e88ca5b56ecd0
Julian Panetta <15096451+jpanetta@users.noreply.github.com>
2015-11-05 14:42:16 -0500

    Updated circular_sector_homog plotting scripts

--- experiments/circular_sector_homog/plot.sh
+++ experiments/circular_sector_homog/plot.sh
@@ -1,8 +1,16 @@
 for i in results/skip_*; do
-    for mod in {0..3}; do
-        gnuplot -e "run_dir='$i'; modulus=$mod" plot_modulus_convergence.gpi
+    cornerAngle=$(head -n1 $i/deg_1.txt | cut -f2)
+    for mod in {Ex,Ey,nu_yx,mu_xy}; do
+        gnuplot -e "data_source='< ./errorData.py $i $mod'; name='$mod'; title_suffix=' (Mesh corner angle $cornerAngle)'; png_path='$i/$mod.png'" plot_relerrors.gpi
     done
     for ij in {0..2}; do
-        gnuplot -e "run_dir='$i'; fluctuation=$ij" plot_maxstresses.gpi
+        for sample in {0..1}; do
+            for comp in {x,y,norm}; do
+                gnuplot -e "data_source='< ./errorData.py $i \"w${ij}_$comp[$sample]\"'; name='fluctuation displacement $ij at sample point $sample ($comp)'; title_suffix=' (Mesh corner angle $cornerAngle)'; png_path='$i/w${ij}_$comp.$sample.png'" plot_relerrors.gpi
+            done
+        done
+    done
+    for ij in {0..2}; do
+        gnuplot -e "run_dir='$i'; fluctuation=$ij" plot_maxstrains.gpi
     done
 done
