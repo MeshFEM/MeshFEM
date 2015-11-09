79076e77300d01ec73d2d41cb8a8d83d7e098e0b
Julian Panetta <15096451+jpanetta@users.noreply.github.com>
2015-11-09 11:21:06 -0500

    Manually create element mesh in Mathematica to avoid remeshing in all cases.

--- experiments/mathematica_compare/cantilever_elasticity.m
+++ experiments/mathematica_compare/cantilever_elasticity.m
@@ -1,7 +1,11 @@
-{objPath, femDegree, Young, poisson} = {$ScriptCommandLine[[2]], ToExpression[$ScriptCommandLine[[3]]], ToExpression[$ScriptCommandLine[[4]]], ToExpression[$ScriptCommandLine[[5]]]};
 Needs["NDSolve`FEM`"]
+{objPath, femDegree, Young, poisson} = {$ScriptCommandLine[[2]], ToExpression[$ScriptCommandLine[[3]]], ToExpression[$ScriptCommandLine[[4]]], ToExpression[$ScriptCommandLine[[5]]]};
 objMesh = Import[objPath];
-em = ToElementMesh[objMesh, MeshQualityGoal->"Minimal", "MeshOrder"->femDegree];
+(* Annoyingly, I cannot seem to prevent ToElementMesh from remeshing when passed objMesh directly. It seems I must manually convert it. *)
+pts = MeshCoordinates[objMesh];
+tris = Map[#1[[1]] &, MeshCells[objMesh, 2]]; (* strip off Polygon[] *)
+em = ToElementMesh["Coordinates"->pts, "MeshElements"->{TriangleElement[tris]}];
+em = MeshOrderAlteration[em, femDegree];
 linearElasticity[Y_,nu_] := {Inactive[Div][({{0,-((Y nu)/(1-nu^2))},{-((Y (1-nu))/(2 (1-nu^2))),0}}.Inactive[Grad][v[x,y],{x,y}]),{x,y}]+Inactive[Div][({{-(Y/(1-nu^2)),0},{0,-((Y (1-nu))/(2 (1-nu^2)))}}.Inactive[Grad][u[x,y],{x,y}]),{x,y}],Inactive[Div][({{0,-((Y (1-nu))/(2 (1-nu^2)))},{-((Y nu)/(1-nu^2)),0}}.Inactive[Grad][u[x,y],{x,y}]),{x,y}]+Inactive[Div][({{-((Y (1-nu))/(2 (1-nu^2))),0},{0,-(Y/(1-nu^2))}}.Inactive[Grad][v[x,y],{x,y}]),{x,y}]};
 dirichletBC = DirichletCondition[{u[x,y]==0,v[x,y]==0.},x==-1];
 traction = NeumannValue[-10, x==1];
