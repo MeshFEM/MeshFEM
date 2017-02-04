c08637566dd607a7ca7dc5c5e2cbe36b1ab5a219
Julian Panetta <15096451+jpanetta@users.noreply.github.com>
2017-02-04 15:25:32 -0500

    Updated cantilever mesh generation script.

--- examples/cantilever/gen.sh
+++ examples/cantilever/gen.sh
@@ -1,5 +1,4 @@
-#!/bin/zsh
-for i in {0..4}; do ../../mesh_convert bar3D_quad.obj -q$i bar3D_tri_$i.poly; done
-for i in {0..4}; do tetgen -Y -F -qp -a$((0.125**$i)) bar3D_tri_$i.poly; done
-for i in {0..4}; do ../../mesh_convert bar3D_tri_$i.1.node bar_tet_$i.msh; done
-rm *.node *.poly *.ele
+#!/usr/bin/env zsh
+for i in {0..4}; do
+    ../../tools/grid $((5 * 2**$i))x$((2**$i))x$((2**$i)) -t bar_tet_$i.msh
+done
