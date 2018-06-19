16566b4293ea6a9963d1ef411f986e596245f0f7
Julian Panetta <julian.panetta@gmail.com>
2018-06-19 16:54:25 +0200

    Move diagnostics coloring settings to UseColors.cmake

--- cmake/UseColors.cmake
+++ cmake/UseColors.cmake
@@ -25,11 +25,14 @@
 
 if(CMAKE_COMPILER_IS_GNUCXX)
 	# If GCC >= 4.9, just activate the right option
+    # We enable colorized diagnostics always instead of using "auto" so that
+    # they're still colored when using Ninja.
+    add_definitions(-fdiagnostics-color=always)
 	if(NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9)
 		message(STATUS "GCC >= 4.9 detected, enabling colored diagnostics")
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=auto")
-		set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fdiagnostics-color=auto")
-		set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -fdiagnostics-color=auto")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=always")
+		set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fdiagnostics-color=always")
+		set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -fdiagnostics-color=always")
 		return()
 	endif()
 	# If GCC < 4.9, maybe we can use gccfilter
