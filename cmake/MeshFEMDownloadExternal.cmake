e95b472e7d2ead48eb5774eb218759e958be1fd3
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2018-10-18 14:06:55 -0400

    Quiet noisy CMake output for external projects.

--- cmake/MeshFEMDownloadExternal.cmake
+++ cmake/MeshFEMDownloadExternal.cmake
@@ -1,12 +1,22 @@
 ################################################################################
 include(DownloadProject)
 
+# With CMake 3.8 and above, we can hide warnings about git being in a
+# detached head by passing an extra GIT_CONFIG option
+if(NOT (${CMAKE_VERSION} VERSION_LESS "3.8.0"))
+    set(MESHFEM_EXTRA_OPTIONS "GIT_CONFIG advice.detachedHead=false")
+else()
+    set(MESHFEM_EXTRA_OPTIONS "")
+endif()
+
 # Shortcut function
 function(meshfem_download_project name)
     download_project(
         PROJ         ${name}
         SOURCE_DIR   ${MESHFEM_EXTERNAL}/${name}
         DOWNLOAD_DIR ${MESHFEM_EXTERNAL}/.cache/${name}
+        QUIET
+        ${MESHFEM_EXTRA_OPTIONS}
         ${ARGN}
     )
 endfunction()
