49c655587f16caa064c149705588fe2fbd0c3b40
Julian Panetta <julian.panetta@gmail.com>
2019-12-09 11:46:46 +0100

    Add python bindings for gradient/divergence. Make Laplacian/Mass matrix return full matrix instead of just the upper triangle

--- src/python_bindings/differential_operators.cc
+++ src/python_bindings/differential_operators.cc
@@ -15,18 +15,25 @@ template<size_t _K, size_t _Degree, class _EmbeddingSpace>
 struct DiffOpBindings {
     using Mesh = FEMMesh<_K, _Degree, _EmbeddingSpace>;
     using Real = typename _EmbeddingSpace::Scalar;
-    static void bind(py::module &m) {
-        m.def("laplacian", [](const Mesh &mesh, bool forceP1) {
-            if (forceP1)
-                return Laplacian::construct<1>(mesh);
-            return Laplacian::construct(mesh);
-        }, py::arg("mesh"), py::arg("forceP1") = false);
+    static constexpr int N = _EmbeddingSpace::RowsAtCompileTime;
+    using MXNd = Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>; // RowMajor for compatibility with numpy's default ordering.
 
-        m.def("mass", [](const Mesh &mesh, bool lumped, bool forceP1) {
-            if (forceP1)
-                return MassMatrix::construct<1>(mesh, lumped);
-            return MassMatrix::construct(mesh, lumped);
-        }, py::arg("mesh"), py::arg("lumped") = false, py::arg("forceP1") = false);
+    static void bind(py::module &m) {
+        m.def("laplacian", [](const Mesh &mesh, bool forceP1, bool upperTriOnly) {
+            TripletMatrix<> L;
+            if (forceP1) L = Laplacian::construct<1>(mesh);
+            else         L = Laplacian::construct   (mesh);
+            if (!upperTriOnly) L.reflectUpperTriangle();
+            return L;
+        }, py::arg("mesh"), py::arg("forceP1") = false, py::arg("upperTriOnly") = false);
+
+        m.def("mass", [](const Mesh &mesh, bool lumped, bool forceP1, bool upperTriOnly) {
+            TripletMatrix<> M;
+            if (forceP1) M = MassMatrix::construct<1>(mesh, lumped);
+            else         M = MassMatrix::construct   (mesh, lumped);
+            if (!upperTriOnly) M.reflectUpperTriangle();
+            return M;
+        }, py::arg("mesh"), py::arg("lumped") = false, py::arg("forceP1") = false, py::arg("upperTriOnly") = false);
 
         m.def("bilaplacian", [](const Mesh &mesh, bool forceP1) {
                 TripletMatrix<> Ltrip;
@@ -49,11 +56,36 @@ struct DiffOpBindings {
 
                 return (L * (1.0 / Mdiag.array()).matrix().asDiagonal() * L).eval();
             }, py::arg("mesh"), py::arg("forceP1") = false);
+
+        m.def("gradient", [](const Mesh &mesh, const Eigen::VectorXd &scalarField) {
+                if (_Degree > 1) throw std::runtime_error("Interpolant type bindings unimplemented...");
+                if (scalarField.size() != mesh.numNodes()) throw std::runtime_error("Incorrect scalar field size");
+                Eigen::MatrixXd g(mesh.numElements(), int(N)); // the cast to int prevents an ODR-use-induced linking error.
+                g.setZero();
+                for (const auto &e : mesh.elements())
+                    for (const auto &n : e.nodes())
+                        g.row(e.index()) += scalarField[n.index()] * e->gradPhi(n.localIndex()).average();
+                return g;
+          }, py::arg("mesh"), py::arg("scalarField").noconvert());
+
+        m.def("divergence", [](const Mesh &mesh, Eigen::Ref<const MXNd> vectorField) {
+                if (_Degree > 1) throw std::runtime_error("Interpolant type bindings unimplemented...");
+                if (size_t(vectorField.rows()) != mesh.numElements()) throw std::runtime_error("Incorrect vector field size");
+                Eigen::VectorXd result(mesh.numNodes());
+                result.setZero();
+                for (const auto &e : mesh.elements())
+                    for (const auto &n : e.nodes())
+                        result[n.index()] += vectorField.row(e.index()).dot(e->gradPhi(n.localIndex()).integrate(e->volume()));
+                return result;
+          }, py::arg("mesh"), py::arg("vectorField").noconvert());
     }
 };
 
 PYBIND11_MODULE(differential_operators, m) {
     m.doc() = "Differential operators provided by a FEM discretization";
+
+    py::module::import("mesh");
+    py::module::import("sparse_matrices");
 
     using V3d = Eigen::Matrix<double, 3, 1>;
     using V2d = Eigen::Matrix<double, 2, 1>;
