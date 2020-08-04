0ee0ff36d6173b97dda5bbe36412a6407b03bccf
Julian Panetta <julian.panetta@gmail.com>
2020-08-03 17:55:07 -0700

    Export Eigensolver symbols in dynamic library

--- src/lib/MeshFEM/Eigensolver.hh
+++ src/lib/MeshFEM/Eigensolver.hh
@@ -2,9 +2,12 @@
 #define EIGENSOLVER_HH
 
 #include <MeshFEM/SparseMatrices.hh>
+#include <MeshFEM_export.h>
 
+MESHFEM_EXPORT
 Real largestMagnitudeEigenvalue(const SuiteSparseMatrix &A, Real tol);
 
+MESHFEM_EXPORT
 Eigen::VectorXd negativeCurvatureDirection(CholmodFactorizer &Hshift_inv, const SuiteSparseMatrix &M, Real tol);
 
 #endif /* end of include guard: EIGENSOLVER_HH */
