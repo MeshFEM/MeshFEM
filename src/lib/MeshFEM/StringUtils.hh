b9fe8ea861db83e8b7b77e87f549ed24b20d9510
Davi Colli Tozoni <davi.tozoni@gmail.com>
2020-08-18 14:32:47 -0400

    making string utils visible

--- src/lib/MeshFEM/StringUtils.hh
+++ src/lib/MeshFEM/StringUtils.hh
@@ -3,23 +3,25 @@
 ////////////////////////////////////////////////////////////////////////////////
 #include <vector>
 #include <string>
+
+#include <MeshFEM_export.h>
 ////////////////////////////////////////////////////////////////////////////////
 
 namespace MeshFEM {
 
 	// Convert to lowercase
-	std::string lowercase(std::string data);
+	MESHFEM_EXPORT std::string lowercase(std::string data);
 
 	// Trim a string
-	void trim(std::string &str);
+	MESHFEM_EXPORT void trim(std::string &str);
 
 	// Tests whether a string starts with a given prefix
-	bool startswith(const std::string &str, const std::string &prefix);
+	MESHFEM_EXPORT bool startswith(const std::string &str, const std::string &prefix);
 
 	// Split a string into tokens
-	std::vector<std::string> split(const std::string &str, const std::string &delimiters = " ");
+	MESHFEM_EXPORT std::vector<std::string> split(const std::string &str, const std::string &delimiters = " ");
 
 	// Replace extension after the last "dot"
-	std::string replace_ext(const std::string &filename, const std::string &newext);
+	MESHFEM_EXPORT std::string replace_ext(const std::string &filename, const std::string &newext);
 
 } // namespace MeshFEM
