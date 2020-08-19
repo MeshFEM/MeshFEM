656211b65d356ae229591c7d299735c61c1778f5
Julian Panetta <julian.panetta@gmail.com>
2020-08-18 21:37:22 -0700

    Removed obsolete python/sparse_matrix_utils.py

--- python/sparse_matrix_utils.py
+++
@@ -1,9 +0,0 @@
-import scipy
-import sparse_matrices
-
-def to_scipy(A):
-    if (isinstance(A, sparse_matrices.SuiteSparseMatrix)):
-        return scipy.sparse.csc_matrix((A.Ax, A.Ai, A.Ap))
-    if (isinstance(A, sparse_matrices.TripletMatrix)):
-        return A.compressedColumn()
-    return A
