7dacfc7b6823efea62c03ec448ef2f1cd433bfc6
Julian Panetta <julian.panetta@gmail.com>
2020-08-18 21:45:30 -0700

    Remove redundant benchmarking from periodic_homogenization bindings, MeshFEM_MASTER_PROJECT cmake var

--- src/python_bindings/periodic_homogenization.cc
+++ src/python_bindings/periodic_homogenization.cc
@@ -14,7 +14,6 @@ namespace py = pybind11;
 #include <MeshFEM/LinearElasticity.hh>
 #include <MeshFEM/Utilities/MeshConversion.hh>
 #include <MeshFEM/Utilities/NameMangling.hh>
-#include <MeshFEM/GlobalBenchmark.hh>
 
 template<typename Mesh>
 using ETensor = ElasticityTensor<typename Mesh::Real, Mesh::EmbeddingDimension>;
@@ -195,19 +194,4 @@ PYBIND11_MODULE(periodic_homogenization, m) {
 // #if MESHFEM_BIND_LONG_DOUBLE
 //     addBindings<long double>(m, detail_module);
 // #endif
-
-    ////////////////////////////////////////////////////////////////////////////////
-    // Benchmarking
-    ////////////////////////////////////////////////////////////////////////////////
-    m.def("benchmark_reset", &BENCHMARK_RESET);
-    m.def("benchmark_start_timer_section", &BENCHMARK_START_TIMER_SECTION, py::arg("name"));
-    m.def("benchmark_stop_timer_section",  &BENCHMARK_STOP_TIMER_SECTION,  py::arg("name"));
-    m.def("benchmark_start_timer",         &BENCHMARK_START_TIMER,         py::arg("name"));
-    m.def("benchmark_stop_timer",          &BENCHMARK_STOP_TIMER,          py::arg("name"));
-    m.def("benchmark_report", [](bool includeMessages) {
-            py::scoped_ostream_redirect stream(std::cout, py::module::import("sys").attr("stdout"));
-            if (includeMessages) BENCHMARK_REPORT(); else BENCHMARK_REPORT_NO_MESSAGES();
-        },
-        py::arg("include_messages") = false)
-        ;
 }
