6e5970d6ca18918d8c052f241f038790c348aa0f
Julian Panetta <julian.panetta@gmail.com>
2020-09-30 17:21:44 -0700

    Refactor pythreejs viewer; implement fully headless version

---
+++ python/vis/offscreen_viewer.py
@@ -0,0 +1,68 @@
+from vis.viewer_base import *
+import OffscreenRenderer
+from OffscreenRenderer import video_writer as vw
+
+class OffscreenViewerBase(ViewerBase):
+    def __init__(self, obj, width=512, height=512, textureMap=None, scalarField=None, vectorField=None, transparent=False):
+        self.renderer = OffscreenRenderer.MeshRenderer(width, height) 
+        self.hexColorToFloat = OffscreenRenderer.hexColorToFloat
+        super().__init__(obj, width=width, height=height, textureMap=textureMap, scalarField=scalarField, vectorField=vectorField, transparent=transparent)
+
+    def _setGeometryImpl(self, attrRaw, preserveExisting=False, updateModelMatrix=False, textureMap=None, scalarField=None, vectorField=None, transparent=False):
+        P = attrRaw['position']
+        N = attrRaw['normal']
+        C = attrRaw['color'] if 'color' in attrRaw else OffscreenRenderer.hexColorToFloat('#D3D3D3')
+        F = attrRaw['index'] if 'index' in attrRaw else None
+        self.renderer.setMesh(P, F, N, C)
+
+    # Start recording to an image sequence/video
+    def recordStart(self, path, codec = None):
+        if codec is None:
+            if path[-4:] == '.mp4': codec = vw.Codec.H264
+            else: codec = vw.Codec.ImgSeq
+        self.recorder = vw.MeshRendererVideoWriter(path, self.renderer, codec=codec)
+        self.recorder.writeFrame()
+
+    def isRecording(self): return hasattr(self, 'recorder')
+
+    def recordStop(self):
+        if self.isRecording():
+            del self.recorder
+
+    def update(self, preserveExisting=False, mesh=None, updateModelMatrix=False, textureMap=None, scalarField=None, vectorField=None, transparent=False):
+        super().update(preserveExisting=preserveExisting, mesh=mesh, updateModelMatrix=updateModelMatrix, textureMap=textureMap, scalarField=scalarField, vectorField=vectorField, transparent=transparent)
+        if self.isRecording():
+            self.recorder.writeFrame()
+
+    def setCamera(self, position, up, fovy, aspect, near, far):
+        self.renderer.perspective(fovy, aspect, near, far)
+        self.renderer.lookAt(position, [0.0, 0.0, 0.0], up)
+
+    def setPointLight(self, color, position):
+        r = self.renderer
+        r.lightEyePos = position
+        r.specularIntensity[:] = 0.0
+        r.diffuseIntensity = OffscreenRenderer.hexColorToFloat(color)
+
+    def showWireframe(self, shouldShow = True):
+        self.renderer.lineWidth = 1.0 if shouldShow else 0.0
+
+    def getCameraParams(self):  return self.renderer.getCameraParams()
+    def setCameraParams(self, params): self.renderer.setCameraParams(params)
+
+    def resize(self, width, height): self.renderer.resize(width, height)
+
+    def writeScreenshot(self, path):
+        self.renderer.render()
+        self.renderer.save(path)
+
+    def transformModel(self, position, scale, quaternion): self.renderer.modelMatrix(position, scale, quaternion)
+
+    def makeTransparent(self, color=None): self.renderer.alpha = 0.25
+    def makeOpaque     (self, color=None): self.renderer.alpha = 1.0
+
+    @property
+    def transparentBackground(self): return self.renderer.transparentBackground
+
+    @transparentBackground.setter
+    def transparentBackground(self, yesno): self.renderer.transparentBackground = yesno
