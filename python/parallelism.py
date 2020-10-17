4446bffce5e3d61680da24a3b3e5fe450b543bc8
Julian Panetta <julian.panetta@gmail.com>
2020-10-16 23:27:56 -0700

    Introduce MESHFEM_NUM_THREADS variable for controlling tbb thread count

--- python/parallelism.py
+++ python/parallelism.py
@@ -1,5 +1,6 @@
 from _parallelism import *
 import os as _os
 
-if 'SLURM_CPUS_PER_TASK' in _os.environ:
-    set_max_num_tbb_threads(int(_os.environ['SLURM_CPUS_PER_TASK']))
+if 'MESHFEM_NUM_THREADS' in _os.environ:
+    cpus = int(_os.environ['MESHFEM_NUM_THREADS'])
+    set_max_num_tbb_threads(cpus)
