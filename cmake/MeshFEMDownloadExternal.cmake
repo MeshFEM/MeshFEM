48fa0a9eede9feb08d3005d4832faa0cea4cf8eb
Julian Panetta <julian.panetta@gmail.com>
2020-11-18 12:49:57 -0800

    Update Catch2, OffscreenRenderer

--- cmake/MeshFEMDownloadExternal.cmake
+++ cmake/MeshFEMDownloadExternal.cmake
@@ -26,8 +26,8 @@ endfunction()
 ## Catch2
 function(meshfem_download_catch)
     meshfem_download_project(Catch2
-        URL     https://github.com/catchorg/Catch2/archive/v2.3.0.tar.gz
-        URL_MD5 1fc90ff3b7b407b83057537f4136489e
+	URL     https://github.com/catchorg/Catch2/archive/v2.13.3.tar.gz
+        URL_MD5 57612324e1e0b49dfc8eab68c03f8473
     )
 endfunction()
 
