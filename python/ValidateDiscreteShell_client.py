637a1710f48487eef61671b5c2bdfedcfc1108c1
Roi Poranne <roi.poranne@gmail.com>
2023-11-09 14:51:32 +0200

    Viewer finally works with asyncio - refactoring next

--- python/ValidateDiscreteShell_client.py
+++ python/ValidateDiscreteShell_client.py
@@ -1,33 +1,30 @@
 #%%
-import threading
 import MeshFEM, mesh
 import numpy as np
-import pyvista, pyvistaqt
-pyvista.set_jupyter_backend('client')
 from client_udp import Client, Message
 import asyncio
 import nest_asyncio
-import time
-from threading import Thread
 nest_asyncio.apply()
+import pyvista, pyvistaqt
+pyvista.set_jupyter_backend('trame')
+from qasync import QEventLoop
+
 # %%
+# start the viewer and get the event loop
+plt = pyvistaqt.BackgroundPlotter()
+loop = QEventLoop(plt.app)
+
+client = Client('localhost', 12345)
+loop.run_until_complete(client.connect())
+#%%
 def emit(message,data=None):
     return asyncio.run(client.send_single_message(Message(message,data)))
-
-def func(reply_message):
-    print(reply_message)
-    #vdmesh.points(ds.getVars().reshape((-1, 3))) 
-    #vdmesh = vd.Mesh([m.vertices(), m.elements()])
-
-client = Client('localhost', 12345, func)
-asyncio.run(client.connect())
 
 m = mesh.Mesh('../3rdparty/MeshFEM/misc/examples/meshes/ball.msh', embeddingDimension=3)
 m=m.boundaryMesh()
 vf = {"V":m.vertices().astype(np.float32),"F":m.elements()}
 emit("initialize",vf)
 
-# %%
 def start_stop_server(flag):
     if flag:
         mes = emit("start")
@@ -35,28 +32,37 @@ def start_stop_server(flag):
     else:
         mes = emit("stop")
         print(mes.string)
-
+# %%
 vistamesh = pyvista.PolyData(m.vertices(), np.hstack((np.full((m.numTris(),1), 3), m.elements())))
-plt = pyvistaqt.BackgroundPlotter()
+
 plt.add_checkbox_button_widget(start_stop_server, value = False)
 plt.add_mesh(vistamesh)
 plt.show()
 
 print("done")
+# %%
+flag = True
+async def start_stop_update():
+    global flag
+    while flag:
+        mes = emit("get")
+        if mes is None:
+            break
+        vistamesh.points=mes.data
+        plt.render()
+        await asyncio.sleep(0.1)
 
-wait = input("PRESS ENTER TO CONTINUE.")
+await start_stop_update()
+# %%
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("asyncio").setLevel(logging.DEBUG)
+loop = QEventLoop(plt.app)
+flag = True
+async def test():
+    print("wait")
+    await asyncio.sleep(0.5)
+    print("done")
+loop.run_until_complete(test())
 
-
-
-# # %%
-# flag = True
-# def update():
-#     global flag
-#     while flag:
-#         mess = emit("get")
-#         vistamesh.points = mess.data
-#         time.sleep(0.5)
-
-# thread = Thread(target=update)
-# thread.start()
 # %%
--- python/client_udp.py
+++ python/client_udp.py
@@ -1,17 +1,18 @@
 import asyncio
 import pickle
 from jetblack_datagram import open_udp_connection
+import logging
+
 
 class Message:
     def __init__(self, string, data=None):
         self.string = string
         self.data = data
-
 class Client:
-    def __init__(self, host, port, func):
+    def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.func = func
+        self.event_loop = None
 
     async def connect_and_send_single_message(self, message):
         try:
@@ -20,6 +21,7 @@ class Client:
             self.send_single_message(message)
             self.client.close()
         except ConnectionRefusedError:
+            print("Connection refused")
             return
         return message
     
@@ -30,10 +32,9 @@ class Client:
                 # Prompt user for a message to send
                 self.client.send(pickle.dumps(message))
                 data = await asyncio.wait_for(self.client.recv(), timeout=5)
-                # data = await self.client.recv()
                 message = pickle.loads(data)
-                print(f"Message recieved: {message.string}")
-                print(f"Data recieved: {message.data!r}")
+                logging.debug(f"Message recieved: {message.string}")
+                logging.debug(f"Data recieved: {message.data!r}")
             except asyncio.TimeoutError:
                 print("Timed out waiting for response")
                 return None
@@ -42,30 +43,28 @@ class Client:
                 return None
             return message
     
-    async def connect(self):
-        try:
-            # Connect to the server
-            self.client = await open_udp_connection((self.host, self.port))
-        except ConnectionRefusedError:
-            return
-        return True
+    async def connect(self, loop=None):
+        self.client = await open_udp_connection((self.host, self.port))
+        logging.debug("Connection successful" if self.client else "Connection refused")
     
     async def close(self):
         self.client.close()
+        if self.debug:
+            print("Connection closed")
         return True
+    
+    def  emit(self,message,data=None):
+        if self.event_loop is None:
+            return asyncio.run(self.send_single_message(Message(message,data)))
+        else:
+            return self.event_loop.run_until_complete(self.send_single_message(Message(message,data)))
     
     def __del__(self):
         self.client.close()
         return True
 
 if __name__ == "__main__":
-    def func(reply_message):
-        print(f"Received reply: {reply_message}")
-
-    loop = asyncio.get_event_loop()
-    
-    client = Client('localhost', 12345, func)
-    loop.create_task(client.event_loop())
-    client.send_event("Event 1")
+    client = Client('localhost', 12345)
+    asyncio.run(client.connect())
     # wait for key press
     
