b4190773a443ee379050294017b2e7175c7a43a9
Johnson Hu <xzhhu@ucdavis.edu>
2025-01-16 19:40:40 -0800

    Updated callback in runSYDParam to record prob.lastFactorizationShiftMagnitude

--- python/Benchmark/helper_funcs.py
+++ python/Benchmark/helper_funcs.py
@@ -35,6 +35,7 @@ def runSYDParam(m, max_iter=200, hessian_shift=1e-8, hessian_proj_option='Adapti
     time_history = []
     grad_norm_history = []
     hessian_projected_history = []
+    hessian_shifted_amount_history = []
 
     def customCallback(prob, i):
         it_time = time.time()
@@ -44,6 +45,7 @@ def runSYDParam(m, max_iter=200, hessian_shift=1e-8, hessian_proj_option='Adapti
     
     def customSaveUVCallback(prob, i):
         hessian_projected_history.append(int(prob.hessianWasProjected))
+        hessian_shifted_amount_history.append(prob.lastFactorizationShiftMagnitude)
         uv_fn = 'uv_ravel_'+ 'iter_' + str(i-1)
         uv_arr = uv.getVars()
         np.savez_compressed(os.path.join(uvsave_path, uv_fn), arr=uv_arr)
@@ -87,9 +89,12 @@ def runSYDParam(m, max_iter=200, hessian_shift=1e-8, hessian_proj_option='Adapti
     if uvsave_path is not None:  
         # we saved uv coordinates per-iteration and hessian_projected_history
         hessian_projected_arr = np.array(hessian_projected_history, dtype=int)
+        hessian_shifted_arr = np.array(hessian_shifted_amount_history, dtype=float)
         hp_filename = 'hessian_projected_history.npy'
+        hs_filename = 'hessian_shifted_amount_history.npy'
         np.save(os.path.join(uvsave_path, hp_filename), hessian_projected_arr)
-        print(f"[File] Saved UV '.npz' files and {hp_filename} in {uvsave_path}.")
+        np.save(os.path.join(uvsave_path, hs_filename), hessian_shifted_arr)
+        print(f"[File] Saved UV '.npz' files, {hp_filename}, and {hs_filename} in {uvsave_path}.")
     else:
         bk_dict = benchmark.to_dict()
         time_arr = np.array(time_history) - start_time
