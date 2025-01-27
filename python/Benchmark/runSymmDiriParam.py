4549f714c7dfa0854ae7f62885b4f77e816834f0
xinzhuohu <xinzhuohu@gmail.com>
2025-01-27 15:24:42 -0800

    Fixed options check

--- python/Benchmark/runSymmDiriParam.py
+++ python/Benchmark/runSymmDiriParam.py
@@ -150,8 +150,8 @@ def main():
     hessian_proj_option = sys.argv[4]
     save_uv_option = sys.argv[5]
 
-    if (hessian_proj_option not in ['Adaptive', 'Always', 'Never', 'xbasedAlways', 'AutoDiff' , 'TinyAD']):
-        print("[Error] Usage of <hessian_proj_option>:  Adaptive or Always or Never or xbasedAlways or AutoDiff or TinyAD")
+    if (hessian_proj_option not in ['Adaptive', 'Always', 'Never', 'xbasedAlways', 'AutoDiff' , 'TinyAD', 'SLIM']):
+        print("[Error] Usage of <hessian_proj_option>:  Adaptive or Always or Never or xbasedAlways or AutoDiff or TinyAD or SLIM")
         sys.exit(1)
     
     if (save_uv_option.lower() not in ['yes', 'no', 'both']):
