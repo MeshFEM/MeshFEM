cced60b64a0b30bcf288bccdbca5471fa8df7864
Julian Panetta <julian.panetta@gmail.com>
2025-07-21 15:57:23 -0700

    Implement flip-avoiding step length limiter for injective parametrization.

---
+++ python_bindings/flip_avoiding_step_length.cc
@@ -0,0 +1,15 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+namespace py = pybind11; // NOLINT (work around clang-tidy bug)
+
+#include "../FlipAvoidingStepLength.hh"
+
+PYBIND11_MODULE(flip_avoiding_step_length, m)
+{
+    py::module::import("MeshFEM");
+    py::module::import("py_newton_optimizer");
+
+    py::class_<FlipAvoidingStepLength, FeasibleStepLengthComputer, std::shared_ptr<FlipAvoidingStepLength>>(m, "FlipAvoidingStepLength")
+        .def(py::init<const Eigen::MatrixXi &>(), py::arg("F"))
+        ;
+}
