674d03ce75ff1816ca6e150899b67363730fd66c
Julian Panetta <julian.panetta@gmail.com>
2025-08-19 14:08:32 -0700

    CommonNeoHookean: implement analytical Hessian projection and more efficient Hessian formulas.

--- src/lib/MeshFEM/EnergyDensities/CommonNeoHookean.hh
+++ src/lib/MeshFEM/EnergyDensities/CommonNeoHookean.hh
@@ -49,6 +49,84 @@ struct CommonNeoHookeanEnergy : public Concepts::NeoHookeanEnergy {
         if (elevel < EvalLevel::Gradient) return;
         m_Finv = m_F.inverse();
         m_Finv_T = m_Finv.transpose();
+
+        if (elevel < EvalLevel::Hessian) return;
+
+        using VMap = Eigen::Map<const VecN_T<Real, N * N>>;
+        using MMap = Eigen::Map<Matrix>;
+        Hessian &H = m_d2energy;
+        if (elevel == EvalLevel::HessianWithDisabledProjection) {
+            // Unprojected Hessian
+            H = m_lambda * VMap(m_Finv_T.data()) * VMap(m_Finv_T.data()).transpose();
+
+            Real coeff = m_mu - m_lambda * m_logDetF;
+            for (size_t j = 0; j < N; ++j)
+                for (size_t i = 0; i < N; ++i)
+                    MMap(H.col(i + j * N).data()) += coeff * m_Finv_T.col(j) * m_Finv.col(i).transpose();
+
+            H.diagonal().array() += m_mu;
+        }
+        else {
+            // Analytical Hessian projection
+            Eigen::JacobiSVD<Matrix> svd;
+            svd.compute(m_F, Eigen::ComputeFullU | Eigen::ComputeFullV);
+            const Matrix &U = svd.matrixU();
+            const Matrix &V = svd.matrixV();
+            const VecN_T<Real, N> &sigma = svd.singularValues();
+
+            auto sigma_inv = (1.0 / sigma.array()).matrix().eval();
+
+            Real c = m_lambda * m_logDetF - m_mu;
+
+            Matrix A;
+            A.setConstant(m_lambda);
+            A.diagonal().array() -= c;
+            Eigen::SelfAdjointEigenSolver<Matrix> eig(sigma_inv.asDiagonal() * A * sigma_inv.asDiagonal());
+
+            Eigen::Matrix<Real, N * N, N> flattenedScalingBasis;
+            for (size_t j = 0; j < N; ++j)
+                MMap(flattenedScalingBasis.col(j).data()) = U.col(j) * V.col(j).transpose();
+
+            Eigen::Matrix<Real, N * N, N> flattenedEigenmatrices = flattenedScalingBasis * eig.eigenvectors();
+
+            H = flattenedEigenmatrices * (eig.eigenvalues().array() + m_mu).matrix().cwiseMax(0.0).asDiagonal() * flattenedEigenmatrices.transpose();
+
+            for (size_t j = 0; j < N; ++j) {
+                for (size_t i = j + 1; i < N; ++i) {
+                    Matrix ui_o_vj = U.col(i) * V.col(j).transpose();
+                    Matrix uj_o_vi = U.col(j) * V.col(i).transpose();
+
+                    Matrix T = ui_o_vj - uj_o_vi; // "Twist" eigenmatrix (unnormalized)
+                    Matrix L = ui_o_vj + uj_o_vi; // "Flip"  eigenmatrix (unnormalized)
+                    Real inv_sigma_product = sigma_inv[i] * sigma_inv[j];
+
+                    Real lambda_T = m_mu + c * inv_sigma_product;
+                    Real lambda_L = m_mu - c * inv_sigma_product;
+                    if (lambda_T > 0) H += VMap(T.data()) * VMap(T.data()).transpose() * (lambda_T * 0.5);
+                    if (lambda_L > 0) H += VMap(L.data()) * VMap(L.data()).transpose() * (lambda_L * 0.5);
+                }
+            }
+
+#if 0
+            {
+                // Validate against brute-force projection.
+                Hessian H_unproj = m_lambda * VMap(m_Finv_T.data()) * VMap(m_Finv_T.data()).transpose();
+
+                Real coeff = m_mu - m_lambda * m_logDetF;
+                for (size_t j = 0; j < N; ++j)
+                    for (size_t i = 0; i < N; ++i)
+                        MMap(H_unproj.col(i + j * N).data()) += coeff * m_Finv_T.col(j) * m_Finv.col(i).transpose();
+
+                H_unproj.diagonal().array() += m_mu;
+
+                Eigen::SelfAdjointEigenSolver<Hessian> Hes(H_unproj);
+                Hessian H_brute_proj = Hes.eigenvectors() * Hes.eigenvalues().cwiseMax(0.0).asDiagonal() * Hes.eigenvectors().transpose();
+
+                std::cout << "Brute-force projection vs analytical projection relative error: " << (H_brute_proj - H).norm() / H_brute_proj.norm() << std::endl;
+                std::cout << "Relative change from projection: " << (H_brute_proj - H_unproj).norm() / H_unproj.norm() << std::endl;
+            }
+#endif
+        }
     }
 
     const Matrix &getDeformationGradient() const { return m_F; }
@@ -64,16 +142,17 @@ struct CommonNeoHookeanEnergy : public Concepts::NeoHookeanEnergy {
 
     template<typename Mat_>
     Matrix delta_denergy(const Mat_ &dF) const {
-        Matrix dF_mat = dF.matrix();
-        return m_mu * dF_mat
-            - (m_lambda * m_logDetF - m_mu) * m_Finv_T * dF_mat.transpose() * m_Finv_T
-            + m_lambda * doubleContract(m_Finv_T, dF) * m_Finv_T;
+        return applyFlattened4thOrderTensor(d2energy(), dF);
+        // Matrix dF_mat = dF.matrix();
+        // return m_mu * dF_mat
+        //     - (m_lambda * m_logDetF - m_mu) * m_Finv_T * dF_mat.transpose() * m_Finv_T
+        //     + m_lambda * doubleContract(m_Finv_T, dF) * m_Finv_T;
         // return m_mu * (dF_mat + m_Finv_T * dF_mat.transpose() * m_Finv_T)
         //          - m_lambda * m_logDetF * m_Finv_T * dF_mat.transpose() * m_Finv_T
         //          + m_lambda * doubleContract(m_Finv_T, dF) * m_Finv_T;
     }
 
-    Real d2energy(const Matrix& dF_a, const Matrix& dF_b) const {
+    Real d2energy(const Matrix &dF_a, const Matrix &dF_b) const {
         return doubleContract(dF_a, delta_denergy(dF_b));
     }
 
@@ -82,12 +161,8 @@ struct CommonNeoHookeanEnergy : public Concepts::NeoHookeanEnergy {
         throw std::runtime_error("Unimplemented.");
     }
 
-    // using Hessian = Eigen::Matrix<Real, N * N, N * N>;
-    // Hessian d2energy() const {
-    //     Hessian H;
-    //     H.setZero();
-    //     return H;
-    // }
+    using Hessian = Eigen::Matrix<Real, N * N, N * N>;
+    const Hessian &d2energy() const { return m_d2energy; }
 
     Matrix PK2Stress() const { return m_Finv_T.transpose() * denergy(); }
 
@@ -97,7 +172,8 @@ private:
 
     // Cached deformation quantities.
     Matrix m_F, m_Finv, m_Finv_T;
-    Real m_detF = 1.0, m_logDetF = 0.0;
+    Real m_detF, m_logDetF;
+    Hessian m_d2energy;
 };
 
 #endif // COMMONNEOHOOKEAN_HH
