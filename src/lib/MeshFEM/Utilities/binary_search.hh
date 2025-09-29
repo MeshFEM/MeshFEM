4c5d0ef8ef8ab922dd5d1fd86ba5484f1a1076ed
Julian Panetta <julian.panetta@gmail.com>
2025-09-29 16:24:07 -0700

    Improvements to "branchless" binary search: ensure cmov generation.

--- src/lib/MeshFEM/Utilities/binary_search.hh
+++ src/lib/MeshFEM/Utilities/binary_search.hh
@@ -2,7 +2,8 @@
 #define BINARY_SEARCH_HH
 #include <functional> // for std::less
 
-// Branchless binary search adapted from https://mhdm.dev/posts/sb_lower_bound/
+// "Branchless" binary search adapted from https://mhdm.dev/posts/sb_lower_bound/
+// with improvements to ensure CMOV generation.
 // (drop-in replacement for std::lower_bound)
 template <class ForwardIt, class T, class Compare = std::less<T>>
 constexpr ForwardIt sb_lower_bound(ForwardIt first, ForwardIt last, const T& value, Compare comp = Compare{}) {
@@ -10,10 +11,15 @@ constexpr ForwardIt sb_lower_bound(ForwardIt first, ForwardIt last, const T& val
     auto length = last - first;
     while (length > 0) {
         auto half = length / 2;
-        if (comp(first[half], value)) {
-            // length - half equals half + length % 2
+#if defined(__clang__)
+        // On recent versions of LLVM/Clang, `__builtin_unpredictable`
+        // suffices to generate a CMOV
+        // https://github.com/llvm/llvm-project/issues/62790
+        if (__builtin_unpredictable(comp(first[half], value)))
             first += length - half;
-        }
+#else
+        first += comp(first[half], value) * (length - half); // This idiom triggers a CMOV on GCC
+#endif
         length = half;
     }
     return first;
