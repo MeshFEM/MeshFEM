8dce9e4ff0c73cd3f2379c69d1bd1a9e0ccc0777
Julian Panetta <julian.panetta@gmail.com>
2025-10-20 12:08:24 -0700

    AutodiffElement: stop pre-computing energy/gradient/Hessian upon each var update
    While this initially seemed like a good idea (energy and gradients are obtained
    "for free" in the AD evaluation of the Hessian), it leads to slow variable
    updates since most of the Hessian evaluation work is done every time the
    variables change. This compromises the early-exit in the final Newton iteration
    and is especially problematic if backtracking iterations are needed.
    
    It also complicates the benchmarking of gradient/Hessian, since most of the
    work was actually done in `setVars`.
    
    In the future, it could be helpful to implement a combined
    energy/gradient/Hessian evaluation for NewtonMultiobjectiveProblem to avoid
    recomputation. But even this cannot be used while keeping our early-exit
    strategy.

--- src/lib/MeshFEM/Elements/AutodiffElement.hh
+++ src/lib/MeshFEM/Elements/AutodiffElement.hh
@@ -41,10 +41,8 @@ struct AutodiffElement : public ElementBase<AutodiffElement<ElementEnergy>>, pri
     using LocalVars = typename ElementEnergy::LocalVars;
 
     template<class Mesh>
-    AutodiffElement(size_t ei, const Mesh &m, const LocalVars &x, MaterialAssignment<MaterialBase> &materials)
-        : Base(ei, materials), ElementEnergy(ei, m) {
-        setDeformedConfiguration(x);
-    }
+    AutodiffElement(size_t ei, const Mesh &m, MaterialAssignment<MaterialBase> &materials)
+        : Base(ei, materials), ElementEnergy(ei, m) { }
 
     static std::string name() {
         if constexpr (has_name_method<ElementEnergy>::value) {
@@ -54,7 +52,7 @@ struct AutodiffElement : public ElementBase<AutodiffElement<ElementEnergy>>, pri
         }
     }
 
-    static constexpr bool CachesDeformedQuantities = true;
+    static constexpr bool CachesDeformedQuantities = false;
     static constexpr size_t NumLocalVars = LocalVars::ColsAtCompileTime * LocalVars::RowsAtCompileTime;
     static_assert(NumLocalVars > 0, "LocalVars must be a non-empty, fixed-sized matrix.");
 
@@ -72,60 +70,48 @@ struct AutodiffElement : public ElementBase<AutodiffElement<ElementEnergy>>, pri
             return j * LocalVars::RowsAtCompileTime + i;
     }
 
-    void setDeformedConfiguration(const LocalVars &x, EvalLevel elevel = EvalLevel::Full) {
-        if (elevel == EvalLevel::EnergyOnly) { m_energy = m_evalWrapper(x); }
-        if (elevel >= EvalLevel::Gradient)   {
-            Eigen::Matrix<ADScalar, LocalVars::RowsAtCompileTime, LocalVars::ColsAtCompileTime> x_AD;
-            for (size_t j = 0; j < LocalVars::ColsAtCompileTime; ++j) {
-                for (size_t i = 0; i < LocalVars::RowsAtCompileTime; ++i) {
-                    x_AD(i, j).value() = x(i, j);
-                    x_AD(i, j).derivatives().setUnit(flattened_index(i, j));
-                }
-            }
+    Real energy(const LocalVars &x) const { return m_evalWrapper(x); }
 
-            ADScalar e_AD = m_evalWrapper(x_AD);
-            m_energy = e_AD.value();
-            m_gradient = e_AD.derivatives();
-        }
-        if (elevel >= EvalLevel::Hessian)   {
-            Eigen::Matrix<AD2Scalar, LocalVars::RowsAtCompileTime, LocalVars::ColsAtCompileTime> x_AD2;
-            for (size_t j = 0; j < LocalVars::ColsAtCompileTime; ++j) {
-                for (size_t i = 0; i < LocalVars::RowsAtCompileTime; ++i) {
-                    x_AD2(i, j).value() = x(i, j);
-                    x_AD2(i, j).value().derivatives().setUnit(flattened_index(i, j)); // Initial derivative of the value
-                    x_AD2(i, j).derivatives()        .setUnit(flattened_index(i, j)); // Initial value of the derivative
-                    // Initial Hessian is zero
-                    for (size_t l = 0; l < LocalVars::ColsAtCompileTime; ++l)
-                        for (size_t k = 0; k < LocalVars::RowsAtCompileTime; ++k)
-                            x_AD2(i, j).derivatives()[flattened_index(k, l)].derivatives().setZero();
-                }
-            }
-
-            AD2Scalar e_AD2 = m_evalWrapper(x_AD2);
-            m_energy = e_AD2.value().value();
-
-            for (size_t i = 0; i < NumLocalVars; ++i) {
-                m_gradient[i] = e_AD2.derivatives()[i].value();
-
-                for (size_t j = 0; j < NumLocalVars; ++j)
-                    m_hessian(i, j) = e_AD2.derivatives()[i].derivatives()[j];
+    Gradient gradient(Real w, const LocalVars &x) const {
+        Eigen::Matrix<ADScalar, LocalVars::RowsAtCompileTime, LocalVars::ColsAtCompileTime> x_AD;
+        for (size_t j = 0; j < LocalVars::ColsAtCompileTime; ++j) {
+            for (size_t i = 0; i < LocalVars::RowsAtCompileTime; ++i) {
+                x_AD(i, j).value() = x(i, j);
+                x_AD(i, j).derivatives().setUnit(flattened_index(i, j));
             }
         }
+        ADScalar e_AD = m_evalWrapper(x_AD);
+        return w * e_AD.derivatives();
     }
 
-    Real energy() const { return m_energy; }
-    Gradient gradient(Real w) const { return w * m_gradient; }
-    Hessian  hessian(Real w, bool /* project */)  const { return w * m_hessian;  }
+    Hessian hessian(Real w, bool /* project */, const LocalVars &x) const {
+        Eigen::Matrix<AD2Scalar, LocalVars::RowsAtCompileTime, LocalVars::ColsAtCompileTime> x_AD2;
+        for (size_t j = 0; j < LocalVars::ColsAtCompileTime; ++j) {
+            for (size_t i = 0; i < LocalVars::RowsAtCompileTime; ++i) {
+                x_AD2(i, j).value() = x(i, j);
+                x_AD2(i, j).value().derivatives().setUnit(flattened_index(i, j)); // Initial derivative of the value
+                x_AD2(i, j).derivatives()        .setUnit(flattened_index(i, j)); // Initial value of the derivative
+                // Initial Hessian is zero
+                for (size_t l = 0; l < LocalVars::ColsAtCompileTime; ++l)
+                    for (size_t k = 0; k < LocalVars::RowsAtCompileTime; ++k)
+                        x_AD2(i, j).derivatives()[flattened_index(k, l)].derivatives().setZero();
+            }
+        }
+
+        AD2Scalar e_AD2 = m_evalWrapper(x_AD2);
+        Hessian H;
+        for (size_t j = 0; j < NumLocalVars; ++j)
+            for (size_t i = 0; i <= j; ++i) {
+                H(i, j) = e_AD2.derivatives()[i].derivatives()[j];
+        }
+        return w * H;
+    }
 
 private:
-    Real m_energy = 0.0;
-    Gradient m_gradient = Gradient::Zero();
-    Hessian  m_hessian  = Hessian::Zero();
-
     // Wrapper that conditionally passes a material object if
     // `ElementEnergy::eval` expects one.
     template<class LVars>
-    auto m_evalWrapper(const LVars &x) {
+    auto m_evalWrapper(const LVars &x) const {
         if constexpr (!std::is_same_v<Material, MaterialBase>) {
             // If `ElementEnergy` specifies a nontrivial material, it must be
             // passed to `eval`.
