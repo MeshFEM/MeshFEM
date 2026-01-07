ca06b464f7e181c3915d68cfe5714d6209b5a6b4
Julian Panetta <julian.panetta@gmail.com>
2026-01-06 16:28:55 -0800

    Demo singular-value-based energy densities (ARAP, symmetric ARAP)

--- python_bindings/custom_energy_demo.cc
+++ python_bindings/custom_energy_demo.cc
@@ -8,14 +8,33 @@ namespace py = pybind11; // NOLINT (work around clang-tidy bug)
 template<typename Real_, size_t Dim_>
 using CommonNeoHookeanAD = AutodiffEDensity<CommonNeoHookeanPsi, Real_, Dim_>;
 
+struct ARAP {
+    template<class Vec>
+    auto psi(const Vec &sigma) { return 0.5 * (sigma.array() - 1).square().sum(); }
+};
+
+struct SymmetricARAP {
+    template<class Vec>
+    typename Vec::Scalar psi(const Vec &sigma) {
+        if (sigma[sigma.size() - 1] < 0) return std::numeric_limits<double>::infinity();
+        return 0.5 * ((      sigma.array() - 1).square().sum()
+                   +  (1.0 / sigma.array() - 1).square().sum());
+    }
+};
+
 PYBIND11_MODULE(custom_energy_demo, m)
 {
     CustomEnergyInstantiationsBinder b(m);
 
-    b.bindParameterless<SymmetricDirichletFromInvariants, false, false, true>("SymmetricDirichletFromInvariants");
-    b.bindParameterless<SymmetricDirichletIsotropicFAD,   false, false, true>("SymmetricDirichletIsotropicFAD");
+    // Parametrization-only energies (`bindSolid = false`)
+    b.bindParameterless<APADIsotropicEDensity_Sigma<ARAP>, false>();
+    b.bindParameterless<APADIsotropicEDensity_Sigma<SymmetricARAP>, false>();
+    b.bindParameterless<APADIsotropicEDensity_SBased<SymmetricDirichletFromInvariants>, false>("SymmetricDirichletFromInvariants");
+    b.bindParameterless<APADIsotropicEDensity_FBased<SymmetricDirichletPsi>,            false>("SymmetricDirichletIsotropicFAD");
 
-    b.bindYoungPoisson<CommonNeoHookeanFromInvariants, true, false, true>("CommonNeoHookeanFromInvariants");
-    b.bindYoungPoisson<CommonNeoHookeanIsotropicFAD,   true, false, true>("CommonNeoHookeanIsotropicFAD");
-    b.bindYoungPoisson<CommonNeoHookeanAD,             true, false, true>("CommonNeoHookeanAD");
+    // Simulation and parametrization energies
+    b.bindYoungPoisson<APADIsotropicEDensity_SBased<CommonNeoHookeanFromInvariants>>("CommonNeoHookeanFromInvariants");
+    b.bindYoungPoisson<APADIsotropicEDensity_FBased<CommonNeoHookeanPsi>           >("CommonNeoHookeanIsotropicFAD");
+    b.bindYoungPoisson<           ADEDensity_FBased<CommonNeoHookeanPsi>           >("CommonNeoHookeanAD");
+
 }
