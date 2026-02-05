c7d7d4d1222eef68428ba39ca44d3733d503b874
Julian Panetta <julian.panetta@gmail.com>
2026-02-04 21:55:57 -0800

    corrections to stepinParamExtrapolation
    - The rows of the sparse matrix returned by `igl.grad` were not ordered
      in the way we originally thought (the first gradient component of all
      faces are collected first, followed by the second, ...).
    - The extrapolation formulas we used were valid only around F = I.

--- python/Stretch2Relax/extra_utils.py
+++ python/Stretch2Relax/extra_utils.py
@@ -15,62 +15,76 @@ import parametrization, benchmark
 import energy, poisson_gradient_integration
 import numpy as np
 import igl
+import differential_operators
 
 import param_utils
 
 
-def stepinParamExtrapolation(m, step, param, alpha, LFactorizer, 
+def stepinParamExtrapolation(m, step, param, alpha, LFactorizer,
                              onlyScaleDGrad=False, fixedVind=None, fixedUV=None):
     '''
     return (reconstruct) uv after extraploation based on current newton step
-    
+
     m:            rest mesh read by MeshFEM
     step:         a newton step
     param:        MeshFEM parameterization object
     alpha:        extraploation scaling factor
     LFactorizer:  Cholesky Factorizer
     '''
-    
+
     # G matrix using igl's function
     G = igl.grad(m.vertices(), m.elements())
-    d_grad_ori = G @ step.reshape(-1, 2)
+    Gd = G @ step.reshape(-1, 2)
     B_arr = np.array([param.getB(ei) for ei in range(m.numElements())])
-    d_grad = d_grad_ori.reshape(m.numElements(), 3, 2).swapaxes(-1, -2) @ B_arr
-    
-    d_grad_T = np.transpose(d_grad, (0, 2, 1))
-    e = 0.5 * (d_grad_T + d_grad)
-    w = 0.5 * (d_grad_T - d_grad)
-    
+    d_grad = Gd.reshape(m.numElements(), 3, 2, order='F').swapaxes(-1, -2) @ B_arr
+
+    # d_grad_T = np.transpose(d_grad, (0, 2, 1))
+    # e = 0.5 * (d_grad_T + d_grad)
+    # w = 0.5 * (d_grad_T - d_grad)
+
     # current deformation gradient F and its polar decompositions
     F = np.array([param.elementJacobian(ei) for ei in range(m.numElements())])
-    
+
+    import energy, tensors
+    crle = energy.CorotatedLinearElastic(tensors.ElasticityTensor2D(1, 0.3))
+
     if onlyScaleDGrad:
         F_extra = F + alpha * d_grad
     else:
-        # Extrapolation
-        R, S = param_utils.polar_decomposition(F)
-        ## extraploate R
-        theta = alpha * w[:, 1, 0] # bottom left entry in w
-        c = np.cos(theta)
-        s = np.sin(theta)
-        R_extra = np.empty((theta.shape[0], 2, 2), dtype=theta.dtype)
-        R_extra[:, 0, 0] = c
-        R_extra[:, 0, 1] = -s
-        R_extra[:, 1, 0] = s
-        R_extra[:, 1, 1] = c
-        R_extra =  R_extra @ R 
-        ## extrapolate S
-        S_extra = S + alpha * e
-        F_extra = R_extra @ S_extra
-    
+        F_extra = []
+        for ei in range(len(F)):
+            crle.setDeformationGradient(F[ei])
+            R = crle.R()
+            dR = crle.delta_R(d_grad[ei])
+            dS = crle.delta_S(d_grad[ei])
+            theta = alpha * (R.T @ dR)[1, 0]
+            c = np.cos(theta)
+            s = np.sin(theta)
+            F_extra.append(R @ np.array([[c, -s], [s, c]]) @ (crle.S() + alpha * dS))
+        # # Extrapolation
+        # R, S = param_utils.polar_decomposition(F)
+        # ## extrapolate R
+        # theta = alpha * w[:, 1, 0] # bottom left entry in w
+        # c = np.cos(theta)
+        # s = np.sin(theta)
+        # R_extra = np.empty((theta.shape[0], 2, 2), dtype=theta.dtype)
+        # R_extra[:, 0, 0] = c
+        # R_extra[:, 0, 1] = -s
+        # R_extra[:, 1, 0] = s
+        # R_extra[:, 1, 1] = c
+        # R_extra =  R_extra @ R
+        # ## extrapolate S
+        # S_extra = S + alpha * e
+        # F_extra = R_extra @ S_extra
+
     F_extra = F_extra @ np.transpose(B_arr, (0, 2, 1))
-    
-    # Solve Poission Equation
+
+    # Solve Poisson Equation
     F_extra_u = F_extra[:, 0, :]
     F_extra_v = F_extra[:, 1, :]
     rhs_u = poisson_gradient_integration.rhs(m, F_extra_u)
     rhs_v = poisson_gradient_integration.rhs(m, F_extra_v)
-    
+
     # Pin One Vertex and Solve rhs
     rhs_u_reduce = rhs_u[1:]
     rhs_v_reduce = rhs_v[1:]
@@ -78,11 +92,11 @@ def stepinParamExtrapolation(m, step, param, alpha, LFactorizer,
     v_sol_reduce = LFactorizer.solve(rhs_v_reduce)
     u_sol = np.concatenate(([0], u_sol_reduce))
     v_sol = np.concatenate(([0], v_sol_reduce))
-    
+
     uv_new = np.column_stack((u_sol, v_sol))
-    
+
     # pass a vertex index to fix
     if fixedVind is not None:
         uv_new = uv_new + (fixedUV - uv_new[fixedVind])
-    
+
     return uv_new, F_extra, d_grad
