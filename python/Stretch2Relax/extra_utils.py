5260bf86511ace9debe11e4c48c936d5a65a0dd8
Johnson Hu <xzhhu@ucdavis.edu>
2026-02-08 15:55:49 -0800

    Refactor extrapolate functions

--- python/Stretch2Relax/extra_utils.py
+++ python/Stretch2Relax/extra_utils.py
@@ -20,65 +20,24 @@ import differential_operators
 import param_utils
 
 
-def stepinParamExtrapolation(m, step, param, alpha, LFactorizer,
-                             onlyScaleDGrad=False, fixedVind=None, fixedUV=None):
-    '''
-    return (reconstruct) uv after extraploation based on current newton step
-
-    m:            rest mesh read by MeshFEM
-    step:         a newton step
-    param:        MeshFEM parameterization object
-    alpha:        extraploation scaling factor
-    LFactorizer:  Cholesky Factorizer
-    '''
-
+def getParamDispGrad(m, step, param):
+    """
+    For Parameterization problem, the displacement gradient field should be (EleNum, 2, 2)
+    """
+    
     # G matrix using igl's function
     G = igl.grad(m.vertices(), m.elements())
     Gd = G @ step.reshape(-1, 2)
     B_arr = np.array([param.getB(ei) for ei in range(m.numElements())])
     d_grad = Gd.reshape(m.numElements(), 3, 2, order='F').swapaxes(-1, -2) @ B_arr
+    
+    return d_grad
 
-    # d_grad_T = np.transpose(d_grad, (0, 2, 1))
-    # e = 0.5 * (d_grad_T + d_grad)
-    # w = 0.5 * (d_grad_T - d_grad)
-
-    # current deformation gradient F and its polar decompositions
-    F = np.array([param.elementJacobian(ei) for ei in range(m.numElements())])
-
-    import energy, tensors
-    crle = energy.CorotatedLinearElastic(tensors.ElasticityTensor2D(1, 0.3))
-
-    if onlyScaleDGrad:
-        F_extra = F + alpha * d_grad
-    else:
-        F_extra = []
-        for ei in range(len(F)):
-            crle.setDeformationGradient(F[ei])
-            R = crle.R()
-            dR = crle.delta_R(d_grad[ei])
-            dS = crle.delta_S(d_grad[ei])
-            theta = alpha * (R.T @ dR)[1, 0]
-            c = np.cos(theta)
-            s = np.sin(theta)
-            F_extra.append(R @ np.array([[c, -s], [s, c]]) @ (crle.S() + alpha * dS))
-        # # Extrapolation
-        # R, S = param_utils.polar_decomposition(F)
-        # ## extrapolate R
-        # theta = alpha * w[:, 1, 0] # bottom left entry in w
-        # c = np.cos(theta)
-        # s = np.sin(theta)
-        # R_extra = np.empty((theta.shape[0], 2, 2), dtype=theta.dtype)
-        # R_extra[:, 0, 0] = c
-        # R_extra[:, 0, 1] = -s
-        # R_extra[:, 1, 0] = s
-        # R_extra[:, 1, 1] = c
-        # R_extra =  R_extra @ R
-        # ## extrapolate S
-        # S_extra = S + alpha * e
-        # F_extra = R_extra @ S_extra
-
-    F_extra = F_extra @ np.transpose(B_arr, (0, 2, 1))
-
+def getUVnewSolvePoission(m, F_extra, LFactorizer, fixedVind=None, fixedUV=None):
+    """
+    F_extra: extrapolated version of deformation gradient field
+    """
+    
     # Solve Poisson Equation
     F_extra_u = F_extra[:, 0, :]
     F_extra_v = F_extra[:, 1, :]
@@ -99,4 +58,92 @@ def stepinParamExtrapolation(m, step, param, alpha, LFactorizer,
     if fixedVind is not None:
         uv_new = uv_new + (fixedUV - uv_new[fixedVind])
 
+    return uv_new
+
+
+def extrapolateDeformGrad(F, alpha, d_grad, method='Eulerian'):
+    """
+    Extrapolate the deformation gradient 
+    
+    F:        element Jacobians get from param class
+    d_grad:   displacement gradient in the orthonomal basis
+    method:   'Lagrangian', 'Eulerian', 'Linear'
+    
+    """
+    
+    if method == 'Eulerian':
+        ## Inverse F using Numpy's batched inverse
+        F_inv = np.linalg.inv(F)
+        DFinv = d_grad @ F_inv 
+        DFinv_T = np.transpose(DFinv, (0, 2, 1))
+
+        R_tilde_zero = 0.5 * (DFinv - DFinv_T)
+        S_tilde_zero = 0.5 * (DFinv + DFinv_T)
+
+        I_tensor = np.broadcast_to(np.eye(2, dtype=F.dtype), F.shape).copy()
+        ## S_extra
+        S_extra = I_tensor + alpha * S_tilde_zero
+        ## R_extra
+        theta = alpha * R_tilde_zero[:, 1, 0] # bottom left entry in w
+        c = np.cos(theta)
+        s = np.sin(theta)
+        R_extra = np.empty((theta.shape[0], 2, 2), dtype=theta.dtype)
+        R_extra[:, 0, 0] = c
+        R_extra[:, 0, 1] = -s
+        R_extra[:, 1, 0] = s
+        R_extra[:, 1, 1] = c
+
+        F_tilde = R_extra @ S_extra
+        F_extra = F_tilde @ F
+    
+    elif method == 'Lagrangian':
+        import energy, tensors
+        crle = energy.CorotatedLinearElastic(tensors.ElasticityTensor2D(1, 0.3))
+        
+        F_extra = []
+        for ei in range(len(F)):
+            crle.setDeformationGradient(F[ei])
+            R = crle.R()
+            dR = crle.delta_R(d_grad[ei])
+            dS = crle.delta_S(d_grad[ei])
+            theta = alpha * (R.T @ dR)[1, 0]
+            c = np.cos(theta)
+            s = np.sin(theta)
+            F_extra.append(R @ np.array([[c, -s], [s, c]]) @ (crle.S() + alpha * dS))
+            
+    elif method == 'Linear':
+        F_extra = F + alpha * d_grad
+        
+    else:  raise RuntimeError(f"Extrapolate method {method} not implemented!")
+    
+    
+    return F_extra
+    
+
+def paramNewtonstepExtrapolation(m, step, param, alpha, LFactorizer,
+                                 method = 'Eulerian',
+                                 fixedVind=None, fixedUV=None):
+    '''
+    return (reconstruct) uv after Eulerian extraploation based on current newton step
+
+    m:            rest mesh read by MeshFEM
+    step:         a newton step
+    param:        MeshFEM parameterization object
+    alpha:        extraploation scaling factor
+    LFactorizer:  Cholesky Factorizer
+    '''
+
+    B_arr = np.array([param.getB(ei) for ei in range(m.numElements())])
+    d_grad = getParamDispGrad(m, step, param)
+    # current deformation gradient F and its polar decompositions
+    F = np.array([param.elementJacobian(ei) for ei in range(m.numElements())])
+    
+    # Extrapolate deformation gradient
+    F_extra = extrapolateDeformGrad(F, alpha, d_grad, method)
+    F_extra = F_extra @ np.transpose(B_arr, (0, 2, 1))
+    
+    # Reconstruct UV Solving Possion equation
+    uv_new = getUVnewSolvePoission(m, F_extra, LFactorizer, fixedVind=fixedVind, fixedUV=fixedUV)
+    
     return uv_new, F_extra, d_grad
+
