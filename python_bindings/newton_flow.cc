363b9eb5f8ef6264e6bc99d43708b90c0bf73f83
Julian Panetta <julian.panetta@gmail.com>
2026-02-07 21:31:39 -0800

    NewtonFlow: implement constant-speed variant.

--- python_bindings/newton_flow.cc
+++ python_bindings/newton_flow.cc
@@ -13,6 +13,7 @@ auto bindNewtonFlow(const std::string &name, py::module &m, py::module &detail) 
     using NFME = NewtonFlowMeshEnergy<Dim, FEMDeg, Psi_>;
     return bindMeshEnergy<NFME>(name, m, detail)
         .def("computeTaylorCoefficients", &NFME::computeTaylorCoefficients, py::arg("hessianFactorization"), py::arg("degree") = 6, py::arg("projectHessian") = false)
+        .def("computeTaylorCoefficientsArclen", &NFME::computeTaylorCoefficientsArclen, py::arg("hessianFactorization"), py::arg("degree") = 6, py::arg("projectHessian") = false)
         ;
 
 }
@@ -23,5 +24,5 @@ PYBIND11_MODULE(newton_flow, m)
     py::module detail = m.def_submodule("detail");
 
     bindNewtonFlow<2, 1, SymmetricDirichlet>("symmetric_dirichlet", m, detail);
-    bindNewtonFlow<2, 1, LinearElasticEnergy>("linear_elastic", m, detail);
+    // bindNewtonFlow<2, 1, LinearElasticEnergy>("linear_elastic", m, detail);
 }
